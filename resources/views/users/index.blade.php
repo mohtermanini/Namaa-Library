182033860f59961274485825e92a519fc3230add
Mohamad Termanini <mohtermanini.job@gmail.com>
2022-03-30 17:08:00 +0300

    Make search area responsive

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,21 +4,13 @@
     <a href="{{ route('users.create') }}" class="btn btn-success">إضافة مستخدم</a>
 
     <div class="row mt-3">
-        <div class="col-4">
-            <form action="{{ route('users.index') }}" method="GET">
-                <div class="row">
-                    <div class="col">
-                        <input type="text" class="form-control" id="searchBox" placeholder="اسم المستخدم..."
-                            autocomplete="off" name="user_name_search" required>
-                        <input type="hidden" value="-1" id="search_value" name="user_id">
-                    </div>
-                    <div class="col-2">
-                        <button class="btn btn-success">بحث</button>
-                    </div>
-                </div>
+        <div class="d-flex col flex-wrap gap-3 align-items-start">
+            <form action="{{ route('users.index') }}" method="GET" class="d-flex gap-3 flex-wrap">
+                <input type="text" class="form-control w-auto" id="searchBox" placeholder="اسم المستخدم..."
+                    autocomplete="off" name="user_name_search" required>
+                <input type="hidden" value="-1" id="search_value" name="user_id">
+                <button class="btn btn-success">بحث</button>
             </form>
-        </div>
-        <div class="col me-2">
             <a href="{{ route('users.index') }}" class="btn btn-success">كل المستخدمين</a>
         </div>
     </div>
